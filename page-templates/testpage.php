c5a393d3edf5cefde960a424912b040f614448e0
bcolchin9611 <bcolchin9611@pointloma.edu>
2018-10-10 12:54:06 -0700

    Initial commit

---
+++ page-templates/testpage.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Template Name: Test Page Template
+ *
+ * Template for displaying test page.
+ *
+ * @package understrap
+ */
+
+get_header();
+?>
+<div class="weaponListContainer">
+  <div class="row">
+    <div class="col-12">S Tier</div>
+    <div class="col-3">Vapr-XKG</div>
+    <div class="col-3">Auger DMR</div>
+    <div class="col-3">Ray Gun</div>
+    <div class="col-3">Titan</div>
+    <div class="col-4">Paladin HB50</div>
+    <div class="col-4">War Machine</div>
+    <div class="col-4">Koshka</div>
+  </div>
+</div>
+
+<?php get_footer(); ?>
